ce4b641ce54f1b83fe67dd5aa8b9a5beac48ce54
Morax0777 <66309010061@cmtc.ac.th>
2024-08-31 15:01:03 +0700

    1

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
-import { comparePassword } from '../lib/auth';
-import { Client } from "pg";
-import dotenv from "dotenv";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { Client } from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -17,40 +16,41 @@ client.connect();
 export async function POST(request) {
   try {
     const { username, password } = await request.json();
+
+    // ตรวจสอบข้อมูลผู้ใช้ในฐานข้อมูล
     const res = await client.query('SELECT * FROM tbl_users WHERE username = $1', [username]);
 
     if (res.rows.length === 0) {
       return new Response(JSON.stringify({ error: 'User not found' }), {
         status: 404,
-        headers: { 'Access-Control-Allow-Origin': '*','Content-Type': 'application/json' },
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
       });
     }
 
     const user = res.rows[0];
     console.log(user);
+
+    // ตรวจสอบรหัสผ่าน
     const match = await bcrypt.compare(password, user.password);
     console.log(match);
 
-    if (match != true) {
+    if (!match) {
       return new Response(JSON.stringify({ error: 'Invalid password' }), {
         status: 401,
-        headers: { 'Access-Control-Allow-Origin': '*','Content-Type': 'application/json' },
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
       });
-    }else{
+    }
 
-    // สมมติว่าเราสร้าง JWT สำหรับการล็อกอิน (สามารถใช้ library เช่น jsonwebtoken)
-    // Generate JWT token
+    // สร้าง JWT token
     const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    // ตัวอย่างนี้จะข้ามขั้นตอนการสร้าง JWT เพื่อความง่าย
     return new Response(JSON.stringify({ message: 'Login successful', user, token }), {
       status: 200,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
 
-    }
   } catch (error) {
-    console.error(error);
+    console.error('Internal Server Error:', error);
     return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
       status: 500,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
